75c8d629f7f720b57a2492776e0c3ccee19fed62
Rishikesh Mane <139518215+RishiMane03@users.noreply.github.com>
2023-09-25 10:14:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here!
+const scrollC = document.getElementById("infi-list")
 
+let i=0;
+function addOneItem() {
+	const li = documnet.createElement("li");
+	li.innerText = `Items ${++i}`;
+	scrollC.appendChild(li);
+}
+
+for (let i = 0; i < 10; i++) {
+	addOneItem();
+}
+
+// code is in jira clone 2 >>> 2:59
